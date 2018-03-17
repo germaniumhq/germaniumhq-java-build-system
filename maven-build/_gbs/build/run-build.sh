66b21f6cd9bc99a9bd3e54ee61de46e4a056ee8b
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-03-17 10:39:21 +0100

    Maven regular build works in GBS.

--- maven-build/_gbs/build/run-build.sh
+++ maven-build/_gbs/build/run-build.sh
@@ -1,1 +1,6 @@
-mvn ${MAVEN_EXTRA_PARAMETERS} --settings /m2/settings.xml clean install
+#!/usr/bin/env bash
+
+env
+
+echo mvn "${MAVEN_EXTRA_PARAMETERS}" --settings /m2/settings.xml clean install
+mvn "${MAVEN_EXTRA_PARAMETERS}" --settings /m2/settings.xml clean install
